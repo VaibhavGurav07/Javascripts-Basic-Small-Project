16ade3755f2ee2c0e618f10adb632f7c14ab389b
Vaibhav Gurav <151390343+VaibhavGurav07@users.noreply.github.com>
2025-04-01 15:43:26 +0000

    update

---
+++ 01_table_2_to_10.js
@@ -0,0 +1,8 @@
+// Table of 2 to 10 using javascripts
+
+for (let i = 0; i < 10; i++){
+    console.log(`table of : ${i}`)
+        for (let j = 0; j <=10; j++) {
+            console.log(i + j + "=" + i*j);                   
+    }
+}
